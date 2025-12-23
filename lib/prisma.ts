4b273ce8d748434978809d4495d3f789d91f3eeb
jotish03 <jotishsharma03@gmail.com>
2025-12-23 14:37:44 +0530

    fixed connection string

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,11 +11,6 @@ const prisma =
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   });
 
 if (process.env.NODE_ENV !== "production") {
